dbfb5b886eea177f30a64ae6f9c2f53253c3bc1f
gafyf <63854933+gafyf@users.noreply.github.com>
2023-11-21 15:36:04 +0100

    Delete chat directory

--- chat/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.utils.translation import gettext as _
-
-
-
-class MessageForm(forms.Form):
-    message = forms.CharField(widget=forms.Textarea, label=_('Message'))
